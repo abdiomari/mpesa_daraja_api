cce00e89d9a3dc6907359f04d92f6e9d4a737f9b
Abdi Omari <74247785+abdiomari@users.noreply.github.com>
2023-01-16 21:00:05 +0300

    cleared api response errors

---
+++ keys.py
@@ -0,0 +1,6 @@
+
+business_shortcode = "174379"
+phone_number = "254794907020"
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key = " aGQn71psarUDdYMTA1nHrZLYHVzZ9c4G"
+consumer_secret = "nSEPmzFnIRTIOgQa"
---
+++ lipanampesa.py
@@ -0,0 +1,58 @@
+import base64
+from datetime import datetime
+# from urllib.request import HTTPBasicAuthHandler
+
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+import keys
+
+
+# gen timestamp
+
+# print(datetime.now())
+# 2023-01-09 19:40:08.611633  ~ 20230109194008 
+unformatted_time = datetime.now()
+formattedtime = unformatted_time.strftime("%Y%m%d%H%M%S")
+# print(formattedtime)
+
+# password generator
+data_to_encode = keys.business_shortcode + keys.lipa_na_mpesa_passkey + formattedtime
+encoded = base64.b64encode(data_to_encode.encode())
+# print(encoded)
+
+decoded = encoded.decode('utf-8')
+
+# print(decoded)
+
+consumer_key = keys.consumer_key
+consumer_secret = keys.consumer_secret
+api_url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+r = requests.get(api_url,  auth=HTTPBasicAuth(consumer_key, consumer_secret))
+print(r.text)
+
+# def lipa_na_mpesa():
+        
+#     access_token = "Access-Token"
+#     api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+#     headers ={"Authorization" : "Bearer %s" % access_token}
+
+#     request = {
+#         "BusinessShortCode":keys.business_shortcode,    
+#         "Password": decoded,    
+#         "Timestamp":formattedtime,    
+#         "TransactionType": "CustomerPayBillOnline",    
+#         "Amount":"1",    
+#         "PartyA":keys.phone_number,    
+#         "PartyB":keys.business_shortcode,    
+#         "PhoneNumber":keys.phone_number,    
+#         "CallBackURL":"https://fullstackdjano.com/lipanampesa/",    
+#         "AccountReference":"Test1",    
+#         "TransactionDesc":"Test1"
+#     }
+
+#     response = requests.post(api_url, json=request, headers = headers)
+#     print(response.text)
+
+# lipa_na_mpesa()
